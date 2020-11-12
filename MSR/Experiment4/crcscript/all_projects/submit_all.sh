0007f0eb9bfb88997728447384e46912875cc274
Jinfeng Lin <ljfnwpusife@gmail.com>
2020-11-11 21:11:53 -0500

    run exp

---
+++ MSR/Experiment4/crcscript/all_projects/submit_all.sh
@@ -0,0 +1,16 @@
+# qsub bk-cmdb.crc
+# qsub canal.crc
+# qsub druid.crc
+# qsub Emmagee.crc
+# qsub nacos.crc
+# qusb ncnn.crc
+# qusb pegasus.crc
+qsub QMui_Android.crc
+# qsub QMUI_iOS.crc
+# qsub rax.crc
+# qsub san.crc
+# qsub weui.crc
+# qsub xLua.crc
+# qsub Cica.crc
+# qsub awb.crc
+# qsub konlpy.crc
